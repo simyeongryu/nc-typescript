fb96525aaae5337bad1f8eaacd8dac4aa115a46a
simyeongryu <rsm0503@gmail.com>
2020-04-12 13:31:06 +0900

    9

--- src/index.ts
+++ src/index.ts
@@ -29,6 +29,14 @@ class Block {
     timestamp: number,
     data: string
   ): string => CryptoJS.SHA256(index + prevHash + timestamp + data).toString();
+
+  // 유효성 검사.
+  static validateStructure = (aBlock: Block): boolean =>
+    typeof aBlock.index === "number" &&
+    typeof aBlock.hash === "string" &&
+    typeof aBlock.prevHash === "string" &&
+    typeof aBlock.timestamp === "number" &&
+    typeof aBlock.data === "string";
 }
 // static 으로 선언되었기 때문에 별도의 인스턴스 생성 없이 바로 사용 가능
 // Block.caculateBlockHash(index, prevHash, timestamp, data);
@@ -43,7 +51,7 @@ const getNewTimestamp = (): number => Math.round(new Date().getTime() / 1000);
 
 const createNewBlock = (data: string): Block => {
   const prevBlock: Block = getLatestBlock();
-  const newIndex: number = prevBlock.index;
+  const newIndex: number = prevBlock.index + 1;
   const newTimestamp: number = getNewTimestamp();
   const newHash: string = Block.caculateBlockHash(
     newIndex,
@@ -60,5 +68,36 @@ const createNewBlock = (data: string): Block => {
   );
   return newBlock;
 };
-console.log(createNewBlock("Hello"), createNewBlock("Bye"));
+
+// 특정 블록의 해쉬를 얻는다.
+const getHashForBlock = (aBlock: Block): string =>
+  Block.caculateBlockHash(
+    aBlock.index,
+    aBlock.prevHash,
+    aBlock.timestamp,
+    aBlock.data
+  );
+
+// block 유효성 검사
+const isBlockValid = (candidateBlock: Block, prevBlock: Block): boolean => {
+  if (!Block.validateStructure(candidateBlock)) {
+    return false;
+  } else if (prevBlock.index + 1 !== candidateBlock.index) {
+    return false;
+  } else if (prevBlock.hash !== candidateBlock.prevHash) {
+    return false;
+  } else if (getHashForBlock(candidateBlock) !== candidateBlock.hash) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+// 블록체인 추가
+const addBlock = (candidateBlock: Block): void => {
+  if (isBlockValid(candidateBlock, getLatestBlock())) {
+    blockchain.push(candidateBlock);
+  }
+};
+
 export {};
